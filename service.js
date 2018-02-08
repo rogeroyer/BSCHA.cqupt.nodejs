f4c0707968ff298f2fc5e1f2000355e19036f66c
Marklar <luojunyi.0@gmail.com>
2018-02-08 14:47:19 +0800

    asdf

--- service.js
+++ service.js
@@ -10,27 +10,21 @@
     ]).then(([database, server]) => {
         if (database && server) {
             console.log('服务正在运行');
-        } else Promise.all([
-            promise(resolve => child_process.exec('npm update', resolve)),
-            promise(resolve => {
-                console.log("启动数据库……");
-                child_process.spawn('neo4j-community-3.3.2\\bin\\neo4j.bat', ['console']).stdout.on('data', data => {
-                    data = data.toString();
-                    if (/.*Remote\sinterface\savailable\sat.*/.test(data)) {
-                        console.log("数据库已启动；");
-                        resolve();
-                    }
-                });
+        } else Promise.every([
+            resolve => {
+                let sp = child_process.spawn('npm.cmd', ['install']);
+                sp.stdout.on('data', data => console.log(data.toString()));
+                sp.on('close', resolve)
+            },
+            resolve => child_process.spawn('neo4j-community-3.3.2\\bin\\neo4j.bat', ['console']).stdout.on('data', data => {
+                data = data.toString();
+                console.log(data);
+                if (/.*Remote\sinterface\savailable\sat.*/.test(data)) resolve();
             }),
-            promise(resolve => {
-                console.log("启动服务器……");
-                child_process.spawn('nnode.bat', ['server.js']).stdout.on('data', data => {
-                    data = data.toString();
-                    if (/.*listening\son\sport.*/.test(data)) {
-                        console.log("服务器已启动；");
-                        resolve();
-                    }
-                });
+            resolve => child_process.spawn('nnode.bat', ['server.js']).stdout.on('data', data => {
+                data = data.toString();
+                console.log(data);
+                if (/.*listening\son\sport.*/.test(data)) resolve();
             })
         ]).then(() => {
             console.log("服务已启动，欢迎使用血液分类系统，请在浏览器打开：http://localhost:3530。");
