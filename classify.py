c250217a59dbe066b85446e10741314edb0dd6c3
CCL <bgasdo36977@gmail.com>
2018-02-09 11:06:18 +0800

    asdf

--- classify.py
+++ classify.py
@@ -59,7 +59,7 @@ def creat_train_set():
             if all_data.shape[0]==0:
                 raise 'No Train Data'
             if ((sum(labels) == len(labels)) | (sum(labels) == 0)):
-                raise 'Add species,plz'
+                raise '目前数据库只有一个物种，请上传多个物种！'
             all_data = pd.concat([pd.DataFrame(labels),all_data],axis=1,ignore_index=True)
             all_data = all_data.fillna(all_data.mean())
     
