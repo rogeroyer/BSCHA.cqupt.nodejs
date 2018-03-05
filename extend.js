ac1800126de320bbf5aba8cfcd4bdeaaf98f81bf
Marklar <luojunyi.0@gmail.com>
2018-03-05 15:22:10 +0800

    asdf

--- extend.js
+++ extend.js
@@ -13,18 +13,24 @@
     'use strict';
 
     global.promise = (...args) => new Promise(...args);
-    Promise.every = function (callbacks) {
-        return new Promise((resolve) => {
-            let results = [],
-                iterate = i => {
-                    if (i < callbacks.length) {
-                        (new Promise(callbacks[i])).then(rtn => {
-                            results.push(rtn);
-                            iterate(i + 1);
-                        });
-                    } else resolve(results);
-                };
-            iterate(0);
-        });
-    };
+
+    Promise.every = callbacks => new Promise(resolve => {
+        let results = [],
+            iterate = i => {
+                if (i < callbacks.length) {
+                    promise(callbacks[i]).then(rtn => {
+                        results.push(rtn);
+                        iterate(i + 1);
+                    });
+                } else resolve(results);
+            };
+        iterate(0);
+    });
+    Promise.some = callbacks => new Promise((resolve, reject) => {
+        let iterate = i => {
+            if (i < callbacks.length - 1) promise(callbacks[i]).then(resolve).catch(() => iterate(i + 1));
+            else if (i < callbacks.length) promise(callbacks[i]).then(resolve).catch(reject);
+        };
+        iterate(0);
+    });
 });
--- service.js
+++ service.js
@@ -11,11 +11,11 @@
         if (database && server) {
             console.log('服务正在运行');
         } else Promise.every([
-            resolve => {
+            /*resolve => {
                 let sp = child_process.spawn('npm.cmd', ['install']);
                 sp.stdout.on('data', data => console.log(data.toString()));
                 sp.on('close', resolve)
-            },
+            },*/
             resolve => child_process.spawn('neo4j-community-3.3.2\\bin\\neo4j.bat', ['console']).stdout.on('data', data => {
                 data = data.toString();
                 console.log(data);
